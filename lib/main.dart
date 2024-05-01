edbcc57516e10033a7ac7d90d79f880aa5c42488
fentahunM <fantahun360@gmial.com>
2024-05-02 00:38:39 +0300

    chat app initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodySmall: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
